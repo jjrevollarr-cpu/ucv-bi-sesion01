3db98c6fdb6624a53dd889ff33de27655746ba54
Katherine <kagabrielco@ucvvirtual.edu.pe>
2026-09-15 19:06:07 -0500

    feat: add staging schema migration

---
+++ changelog/db.changelog-master.sql
@@ -0,0 +1,4 @@
+--changeset estudiante:002
+CREATE SCHEMA IF NOT EXISTS <workspace>.bi_staging_<7002278384>
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+--rollback DROP SCHEMA IF EXISTS <workspace>.bi_staging_<7002278384>;
